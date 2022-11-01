ff62ec39892af348f07bc7535af30634ecde8fbb
ankit <ankitcool4370@gmail.com>
2022-11-01 20:41:34 +0530

    sum of two number

---
+++ Fouthproject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fouthproject
+{
+      class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, sum;
+
+            Console.Write("enter the first number : ");
+            string s1 = Console.ReadLine();
+            x = int.Parse(s1);
+
+            Console.Write("enter the second number : ");
+            string s2 = Console.ReadLine();
+            y = int.Parse(s2);
+
+            sum = x + y;
+
+            Console.WriteLine("the sum of the two number = " + sum);
+            Console.ReadLine();
+        }
+    }
+}
+
